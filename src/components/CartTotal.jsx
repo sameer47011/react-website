dcdeb203f2157e83b4943d6e3a1e78f6f60efa95
sameer47011 <sameer47011@gmail.com>
2024-09-30 12:40:52 +0530

    modified:   src/components/CartTotal.jsx         modified:   src/components/Navbar.jsx         modified:   src/context/ShopContext.jsx         modified:   src/pages/About.jsx         modified:   src/pages/Cart.jsx         modified:   src/pages/Contact.jsx         modified:   src/pages/Login.jsx         modified:   src/pages/Orders.jsx         modified:   src/pages/PlaceOrder.jsx

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -7,7 +7,7 @@ const CartTotal = () => {
   return (
     <div className='w-full'>
       <div className='text-2xl'>
-        <Title text1={'CART'} text2={'TOTAL'} />
+        <Title text1={'CART '} text2={'TOTAL'} />
       </div>
       <div className='flex flex-col gap-2 mt-2 text-sm'>
         <div className='flex justify-between'>
@@ -17,13 +17,13 @@ const CartTotal = () => {
         <hr />
         <div className='flex justify-between'>
           <p>Shipping Fee</p>
-          <p>{currency}{delivery_fee}</p>
+          <p>{currency}{delivery_fee}.00</p>
 
         </div>
         <hr />
         <div className='flex justify-between'>
           <b>Total</b>
-          <b>{currency}{getCartAmount()===0?0:getCartAmount()+delivery_fee}</b>
+          <b>{currency}{getCartAmount()===0?0:getCartAmount()+delivery_fee}.00</b>
 
         </div>
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import Title from '../components/Title'
 import assets from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 const Cart = () => {
-  const { products, currency, cartItems,updateQuantity } = useContext(ShopContext)
+  const { products, currency, cartItems,updateQuantity,navigate } = useContext(ShopContext)
   const [cartData, setCartData] = useState([])
   useEffect(() => {
     const tempData = [];
@@ -56,7 +56,10 @@ const Cart = () => {
 <div className="flex justify-end my-5">
   <div className="w-full sm:w-[450px]">
     <CartTotal/>
+<div className="w-full text-end">
+  <button onClick={()=>navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3">PROCEED TO CHECKOUT</button>
 
+</div>
   </div>
 
 </div>
